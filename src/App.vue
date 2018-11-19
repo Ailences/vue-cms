9fc6399d332b04f80fa1959270331685149aee01
Ailences <xs3646503@gmail.com>
2018-11-19 20:54:24 +0800

    初始化项目,完成首页

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div id="app">
-    <h1>这app模块</h1>
-    <div class="box">
-      <p class="content">lallal</p>
-    </div>
+  <div class="app-container">
+
+	<!-- 头部 -->
+    <mt-header fixed title="固定在顶部"></mt-header>
+
+	<!-- 底部 -->
+	<nav class="mui-bar mui-bar-tab">
+		<router-link class="mui-tab-item" to="/home">
+			<span class="mui-icon mui-icon-home"></span>
+			<span class="mui-tab-label">首页</span>
+		</router-link>
+		<router-link class="mui-tab-item" to="/member">
+			<span class="mui-icon mui-icon-contact"></span>
+			
+			<span class="mui-tab-label">会员</span>
+		</router-link>
+		<router-link class="mui-tab-item" to="/shopcar">
+			<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+				<span class="mui-badge">0</span>
+			</span>
+			<span class="mui-tab-label">购物车</span>
+		</router-link>
+		<router-link class="mui-tab-item" to="/search">
+			<span class="mui-icon mui-icon-search"></span>
+			<span class="mui-tab-label">搜索</span>
+		</router-link>
+	</nav>
+
+	<!-- 内容区域 -->
+	<transition>
+		<router-view></router-view>
+	</transition>
+	
   </div>
 </template>
 
@@ -11,19 +39,24 @@
 </script>
 
 <style lang="less">
-html,
-body {
-  padding: 0;
-  margin: 0;
-}
-.box {
-  width: 200px;
-  height: 200px;
-  background-color: red;
-  p {
-    font-size: 48px;
-    font-weight: 700
+.app-container {
+  padding: 40px 0 50px 0;
+  overflow-x: hidden;
+
+  .v-enter {
+    opacity: 0;
+    transform: translateX(100%);
   }
 
+  .v-leave-to {
+    opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+	  transition: all .6s ease;
+  }
 }
 </style>
---
+++ src/pages/member/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="member-container">
+        member
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+</style>
