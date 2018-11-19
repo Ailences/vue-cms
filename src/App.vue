a44c50536acc423acae6bada1942cd74e784979c
Ailences <xs3646503@gmail.com>
2018-11-19 12:35:54 +0800

    本地新建项目,关联github

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>这app模块</h1>
+    <div class="box">
+      <p class="content">lallal</p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  p {
+    font-size: 48px;
+    font-weight: 700
+  }
+
+}
+</style>
