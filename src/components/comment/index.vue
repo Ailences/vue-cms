468708b6d992a0c1c1ab1df2da66f44d944fc4fc
Ailences <xs3646503@gmail.com>
2018-11-22 19:24:59 +0800

    评论发表、加载更多功能完成

--- src/components/comment/index.vue
+++ src/components/comment/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="comment-container">
     <h2>发表评论</h2>
-    <textarea placeholder="请输入您要评论的内容!最多输入120字" maxlength="120"></textarea>
-    <mt-button type="primary" size="large">发表评论</mt-button>
+    <textarea placeholder="请输入您要评论的内容!最多输入120字" maxlength="120" v-model="msg"></textarea>
+    <mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
     <div class="comment-list">
       <div class="comment-item" v-for="(item, index) in commentList" :key="index">
         <div class="comment-title">第{{index + 1}}楼&nbsp;&nbsp;用户:{{item.user_name}}&nbsp;&nbsp;发表时间:{{item.add_time |
@@ -12,7 +12,7 @@
         </div>
       </div>
     </div>
-    <mt-button type="danger" size="large" plain>加载更多</mt-button>
+    <mt-button type="danger" size="large" plain @click="getMore">加载更多</mt-button>
   </div>
 </template>
 
@@ -23,7 +23,8 @@ export default {
   data() {
     return {
       pageIndex: 1,
-      commentList: []
+      commentList: [],
+      msg: ""
     };
   },
   created() {
@@ -36,9 +37,49 @@ export default {
         .then(result => {
           //   console.log(result.body)
           if (result.body.status === 0) {
-            this.commentList = result.body.message;
+            // this.commentList = result.body.message;
+            this.commentList = this.commentList.concat(result.body.message);
           } else {
             Toast("获取评论信息失败!请重试!");
+          }
+        });
+    },
+    getMore() {
+      this.pageIndex++;
+      this.getComments();
+    },
+    postComment() {
+      if (this.msg.trim().length === 0) {
+        return Toast("评论内容不能为空!");
+      }
+      // this.$http
+      //   .post("api/postcomment/" + this.$route.params.id, {
+      //     content: this.msg.trim()
+      //   })
+      //   .then(result => {
+      //     // console.log(result);
+      //     if (result.body.status === 0) {
+      //       this.pageIndex = 1;
+      //       this.commentList = [];
+      //       this.getComments();
+      //       this.msg = "";
+      //     } else {
+      //       Toast("发表评论失败!请重试!");
+      //     }
+      //   });
+      this.$http
+        .post("api/postcomment/" + this.$route.params.id, {
+          content: this.msg.trim()
+        })
+        .then(result => {
+          if (result.body.status === 0) {
+            var cmt = {
+              user_name: "匿名用户",
+              add_time: Date.now(),
+              content: this.msg.trim()
+            };
+            this.commentList.unshift(cmt);
+            this.msg = "";
           }
         });
     }
@@ -48,6 +89,7 @@ export default {
 
 <style lang="less">
 .comment-container {
+  padding-bottom: 10px;
   h2 {
     margin-bottom: 10px;
   }
